4a71b5c1e4eb8ea1010d1ad85523406b51d3f564
Faweiz <faweiz@gmail.com>
2020-03-16 21:19:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+var express = require('express');
+var http = require('http');
 var mosca = require('mosca');
-var mqttServer = new mosca.Server({
-    port:1883;
+
+var app = express();
+var server = http.createServer(app);
+
+var pubsubsettings = {
+    type: 'mongo',
+    url: 'mongodb://tonyz:abc123456789@ds253871.mlab.com:53871/mqttbrokermosca' || 'mongodb://localhost:27017/app',
+    pubsubCollection: 'mqtt',
+    mongo: {}
+};
+
+var server = new mosca.Server({
+    backend: pubsubsettings,
+    persistence: {
+        factory: mosca.persistence.Mongo,
+        url: 'mongodb://tonyz:123456789@ds051655.mlab.com:51655/apiai-faweizfacebook' || 'mongodb://localhost:27017/app'
+    }
+}, function() {
+    server.attachHttpServer(app);
 });
 
-mqttServer.on("clientConnected", function(client){
-    console.log("client connected", client.id);
-});
-
-mqttServer.on('ready', function(){
-    console.log("MQTT is running...");
+server.on('ready', function() {
+    console.log('Mosca is running');
 });
